97eff244b8024e8748c1380c032ca4b4da64cb5b
Steve MacKinnon <mackinnon.st@gmail.com>
2025-02-11 22:48:45 -0500

    autoclose nvim tree

--- init.lua
+++ init.lua
@@ -4,6 +4,17 @@ vim.g.mapleader = " "
 vim.opt.guicursor = {
   "n-v-c:block,i-ci-ve:ver10,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175",
 }
+
+-- autoclose nvim tree
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
+      vim.cmd "quit"
+    end
+  end
+})
+
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
