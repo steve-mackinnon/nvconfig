67f94615681022df39ef26232a4ea850be5e756a
Steve MacKinnon <mackinnon.st@gmail.com>
2025-02-24 13:47:41 -0500

    markdown preview

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -58,6 +58,15 @@ return {
     event = "VeryLazy",
   },
   {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
+  {
     "yetone/avante.nvim",
     event = "VeryLazy",
     lazy = false,
